d666e2a9a663a1ca62e1846df2bb67766373ee93
Wazir <ahsankamal25@gmail.com>
2023-03-29 16:28:26 +0500

    Finished with another project

--- script.js
+++ script.js
@@ -143,22 +143,88 @@
 
 /////////////////////Bulb Project//////////////////////////
 
-const bulb = document.querySelector('#bulb')  // img
-console.log(bulb)
-const bulbSwitch = document.querySelector('#bulbSwitch') //btn
+// const bulb = document.querySelector('#bulb')  // img
+// console.log(bulb)
+// const bulbSwitch = document.querySelector('#bulbSwitch') //btn
 
-bulbSwitch.addEventListener('click', (event) => {
-    // console.log(bulb.src)
-    if( bulb.src.match('off') ) {
+// bulbSwitch.addEventListener('click', (event) => {
+//     // console.log(bulb.src)
+//     if( bulb.src.match('off') ) {
         
-        bulb.src = './bulb-on.gif'
-        bulbSwitch.innerHTML = "Turn Off"
-    } else {
-        bulb.src = './bulb-off.png'
-        bulbSwitch.innerHTML = "Turn On"
+//         bulb.src = './bulb-on.gif'
+//         bulbSwitch.innerHTML = "Turn Off"
+//     } else {
+//         bulb.src = './bulb-off.png'
+//         bulbSwitch.innerHTML = "Turn On"
+        
+//     }
+// })
+
+
+///////////////////2nd Project ////////////////////////////
+
+    const div = document.querySelector('#root')
+    const button = document.querySelector('button')
+
+    function createitem (item) {
+        const card = document.createElement('div')
+        card.classList.add('card')
+    
+        //photo
+    
+        const photo = document.createElement('img')
+        photo.src = item.thumbnailUrl;
+        
+        const title = document.createElement('h4')
+        title.innerHTML = item.title;
+
+        card.appendChild(photo);
+        card.appendChild(title);
+
+        root.appendChild(card);
         
     }
-})
+
+    
+        function displayImages(items) {
+            items.forEach((item) => {
+                createitem(item)
+            })
+            console.log(items)
+        }
+
+
+    button.addEventListener('click', () => {
+        fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
+        .then(res => res.json())
+        .then(items => {
+            displayImages(items);
+        })
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
